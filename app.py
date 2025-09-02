fd75c8fd099a50989dfb3dadb5e5e8037cd9ef25
leonardoscampos1 <157980058+leonardoscampos1@users.noreply.github.com>
2025-09-02 06:22:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import openpyxl as xl
 
 # --- Configurações ---
-url = "https://whatsappcobranca-production.up.railway.app//message/sendText/enviomensagem"
+url = "https://whatsappcobranca-production.up.railway.app/message/sendText/enviomensagem"
 api_key = "123456"
 numero_remetente = "5521980210457"  # sua instância
 
@@ -77,4 +77,5 @@ if arquivo:
                 st.error(f"❌ {cliente} {numero_dest}: Connection Error: {errc}")
             except requests.exceptions.Timeout as errt:
                 st.error(f"❌ {cliente} {numero_dest}: Timeout Error: {errt}")
+
 
